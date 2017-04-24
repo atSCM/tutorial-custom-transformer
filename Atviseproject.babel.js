ed1b3225a41f841a50cdd1aa454249895c8930ca
Lukas Hechenberger <admin@ls-age.com>
2017-04-24 09:08:49 +0200

    chore: Project setup

---
+++ Atviseproject.babel.js
@@ -0,0 +1,29 @@
+import { Atviseproject } from 'atscm';
+
+/**
+ * atvise-scm configuration of tutorial-custom-transformer.
+ */
+export default class TutorialCustomTransformer extends Atviseproject {
+
+  /**
+   * The atvise-server's host
+   * @type {String}
+   */
+  static get host() {
+    return 'localhost';
+  }
+
+  /**
+   * The atvise-server ports to use.
+   * @type {Object}
+   * @property {Number} opc The OPC-UA port the atvise-server runs on.
+   * @property {Number} http The HTTP port the atvise-server can be reached at.
+   */
+  static get port() {
+    return {
+      opc: 4840,
+      http: 80,
+    };
+  }
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Implementing a custom Transformer
+
+In this document we'll guide you through the steps necessary to implement a custom Transformer.
+The transformer we'll implement will use [Babel](http://babeljs.io) to transpile ES2015/ES6 JavaScript to plain ES5 JavaScript that works in all Browsers.
